bce8eae9fc3b4faa809f461a6f3da870975f014d
André de Paula Terceiro <andre@terceiro.com.br>
2024-12-12 14:25:14 -0300

    feat: test related to a List (ArrayList)

---
+++ tests/list.kt
@@ -0,0 +1,13 @@
+package tests
+
+fun main() {
+    var numeros = ArrayList<Int>()
+    numeros.add(1)
+    numeros.add(2)
+    numeros.add(3)
+
+    print(numeros)
+    print(" -- ")
+    print(numeros[1])
+
+}
